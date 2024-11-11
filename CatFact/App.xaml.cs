8281ae4c72fa2248ddbe8124ebdc56dde81666a2
manderson116 <manderson116@my.wctc.edu>
2024-11-11 15:53:35 -0600

    Add project files.

---
+++ CatFact/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CatFact
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CatFact/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CatFact
+{
+    public partial class MainPage : ContentPage
+    {
+        HttpClient client = new HttpClient();
+
+        public class CatFact
+        {
+            public string fact { get; set; }
+            public int length { get; set; }
+        }
+
+        private async Task GetFact()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                CatFact catFact = JsonConvert.DeserializeObject<CatFact>(json);
+                FactLabel.Text = catFact.fact;
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            GetFact();
+        }
+
+        private void FactBtnClicked(object sender, EventArgs e)
+        {
+            GetFact();
+        }
+    }
+}
